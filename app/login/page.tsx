f1cfd1fcf180144a883556a00d30e10064128935
Hussainforvercel <iqradaud643@gmail.com>
2024-10-10 15:59:54 -0700

    finalized

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "../pages/login";
+import Navbar from "../Components/navbar";
+import Footer from "../Components/footer";
+
+function Page() {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+      <Footer />
+    </div>
+  );
+}
+
+export default Page;
